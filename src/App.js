359b2d9178c372b2b2882a0af4814e370c4d3e25
mompaduck <mompaduck@gmail.com>
2022-10-28 02:21:40 +0900

    realtor clone react

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import Offers from "./pages/Offers"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./pages/ForgotPassword"
+import CreateListing from "./pages/CreateListing"
+import EditListing from "./pages/EditListing"
+import Listing from "./pages/Listing"
+import Category from "./pages/Category"
+
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+import { getAuth } from "firebase/auth"
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+
+          <Route path="edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </div>
+  )
+}
+export default App
